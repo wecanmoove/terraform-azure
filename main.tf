c23d1d0ae59bdf823ada506cdd22fa3e19d756de
wecanmoove <63386275+wecanmoove@users.noreply.github.com>
2026-06-13 15:52:50 +0200

    Add Azure resource group and Kubernetes cluster resources

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  dns_prefix          = var.aks_dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = var.tags
+}
